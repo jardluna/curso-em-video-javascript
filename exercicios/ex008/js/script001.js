c5eabd46a0b74b505afd5cdfb4ba63615760a274
jardluna <134236491+jardluna@users.noreply.github.com>
2024-03-04 21:03:22 -0300

    Atualização

---
+++ exercicios/ex008/js/script001.js
@@ -0,0 +1,11 @@
+var vel = 85
+console.log(`-----------------------------------------------`)
+console.log(`----- VELOCIDADE MÁXIMA PERMITIDA: 80km/h -----`)
+console.log(`-----------------------------------------------`)
+
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+//CONDIÇÃO SIMPLES
+if (vel > 80) {
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança.`)
---
+++ exercicios/ex008/js/script002.js
@@ -0,0 +1,7 @@
+var pais = `Alemanha` //<-- Digite o nome do pais ao lado
+console.log(`Você mora no(a) ${pais}.`)
+if (pais == `Brasil` || pais == `brasil`) {
+    console.log(`Logo, você é Brasileiro!`)
+} else {
+    console.log(`Então você é Estrangeiro!`)
+}
