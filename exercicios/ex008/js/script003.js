5023ef27a3f3392e4db224f23a338b38bfd4632c
jardluna <134236491+jardluna@users.noreply.github.com>
2024-03-05 19:19:36 -0300

    Atualização

---
+++ exercicios/ex008/js/script003.js
@@ -0,0 +1,31 @@
+var idade = 87
+console.log('----------------------------')
+console.log('|| TESTE DE IDADE ||')
+if (idade <= 10) {
+    console.log('Você é uma criança')
+} else if (idade >= 11 && idade <= 17) {
+    console.log('Você ainda é um adolescente')
+} else if (idade >= 18 && idade <= 65) {
+    console.log('Você já é um adulto')
+} else {
+    console.log('Você agora é um idoso')
+}
+console.log('----------------------------')
+
+/*var idade = 87
+console.log('----------------------------')
+console.log('|| TESTE DE IDADE ||')
+if (idade <= 10) {
+    console.log('criança')
+} else {
+    if (idade >= 11 && idade <= 17) {
+        console.log('adolescente')
+    } else {
+        if (idade >= 18 && idade <= 65) {
+            console.log('adulto')
+        } else {
+            console.log('idoso')
+        }
+    }
+}
+console.log('----------------------------')*/
