8dd6a130dd5f43f3b198a4bd43cf1262fc2e1407
jardluna <134236491+jardluna@users.noreply.github.com>
2024-03-11 16:17:43 -0300

    Atualização

---
+++ desafios-propostos/dp001/js/script.js
@@ -0,0 +1,11 @@
+function clicar1() {
+    window.alert('Você clicou no botão 1')
+}
+
+function clicar2() {
+    window.alert('Você clicou no botão 2')
+}
+
+function clicar3() {
+    window.alert('Você clicou no botão 3')
+}
