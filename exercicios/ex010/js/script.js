f95772241a46161030a03949ef272dd0295a466d
jardluna <134236491+jardluna@users.noreply.github.com>
2024-03-11 18:19:33 -0300

    Atualização

---
+++ exercicios/ex010/js/script.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Tudo bem? Estamos na ${c}º contagem`)
+    c++
+} while (c <= 5)
+
+/*while (c <= 5) {
+    console.log(`Tudo bem? Estamos na ${c}º contagem`)
+    c++
+}*/
+
+
+/*console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")*/
