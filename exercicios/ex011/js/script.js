e2ae2968ad9e2d74bf15df121484ce11c852d217
jardluna <134236491+jardluna@users.noreply.github.com>
2024-03-11 18:59:32 -0300

    Atualização

---
+++ exercicios/ex011/js/script.js
@@ -0,0 +1,7 @@
+console.log(`-----------------------`)
+console.log(`------ COMEÇANDO ------`)
+for (c = 1; c <= 5; c++) {
+    console.log(`Teste nº${c}`)
+}
+console.log(`------ TERMINANDO ------`)
+console.log(`------------------------`)
